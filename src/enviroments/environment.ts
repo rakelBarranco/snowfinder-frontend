5dd1b81c977f41093ff6c58281329b24660d55ca
Raquel Barranco <raquelbarranco02@gmail.com>
2026-05-30 13:56:09 +0200

    cambio url back

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://snowfinder-backend-production.up.railway.app'
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://snowfinder-backend-production.up.railway.app'
 };
